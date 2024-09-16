8d4122e6a0f78be843cd17447bac08e41b8d974e
Valery Krikunenko <cat.cosmo00@mail.ru>
2024-09-16 16:48:35 +0300

    Add files via upload

---
+++ scanf2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#define D 2.54
+#define K 2.32166
+#define C 2.7076
+// Задание 2
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int dym;
+	printf("How many?");
+	scanf("%d", &dym);
+	float result = D * dym;
+	printf("%.1f cm\n", result);
+	float resultS = K * dym;
+	printf("%.1f cm\n", resultS);
+	float resultSt = C * dym;
+	printf("%.1f cm\n", resultSt);
+	getchar();
+}
