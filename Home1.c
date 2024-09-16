88a91aed57997a285a8e09066b29d9f00031f5fb
Valery Krikunenko <cat.cosmo00@mail.ru>
2024-09-16 16:34:33 +0300

    Add files via upload

---
+++ Home1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	printf(" /| /| /| /| /|\n");
+	printf(" /| /| /| /| /|\n");
+	printf(" /| /| /| /| /|\n");
+	printf(" /| /| /| /| /|\n");
+	printf(" /| /| /| /| /|\n");
+	printf("  |  |  |  |  |\n");
+	printf("  |  |  |  |  | _\n");
+	printf("__|__|__|__|__|_|_\n");
+	printf("\\________________/\n");
+	printf(" \\______________/\n");
+	printf("  \\____________/\n");
+	printf("   \\__________/\n");
+	getchar();
+}
